7bfded6466b1a61d1a0ad6ead0e8b5c8fdcf4c3e
Joan Fabrégat <joan@codeinc.fr>
2018-03-08 17:47:08 +0100

    adds the session data entity and it's interface

---
+++ DoctrineSessionHandler/SessionData/SessionDataEntity.php
@@ -0,0 +1,93 @@
+<?php
+//
+// +---------------------------------------------------------------------+
+// | CODE INC. SOURCE CODE                                               |
+// +---------------------------------------------------------------------+
+// | Copyright (c) 2017 - Code Inc. SAS - All Rights Reserved.           |
+// | Visit https://www.codeinc.fr for more information about licensing.  |
+// +---------------------------------------------------------------------+
+// | NOTICE:  All information contained herein is, and remains the       |
+// | property of Code Inc. SAS. The intellectual and technical concepts  |
+// | contained herein are proprietary to Code Inc. SAS are protected by  |
+// | trade secret or copyright law. Dissemination of this information or |
+// | reproduction of this material  is strictly forbidden unless prior   |
+// | written permission is obtained from Code Inc. SAS.                  |
+// +---------------------------------------------------------------------+
+//
+// Author:   Joan Fabrégat <joan@codeinc.fr>
+// Date:     08/03/2018
+// Time:     17:43
+// Project:  lib-doctrinesessionhandler
+//
+declare(strict_types = 1);
+namespace CodeInc\DoctrineSessionHandler\SessionData;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * Class SessionDataEntity
+ *
+ * @ORM\Entity()
+ * @package CodeInc\DoctrineSessionHandler
+ * @author Joan Fabrégat <joan@codeinc.fr>
+ */
+class SessionDataEntity implements SessionDataEntityInterface {
+	/**
+	 * @ORM\Column(type="string")
+	 * @var string|null
+	 */
+	private $id;
+
+	/**
+	 * @ORM\Column(type="datetime")
+	 * @var \DateTime|null
+	 */
+	private $lastHit;
+
+	/**
+	 * @ORM\Column(type="text")
+	 * @var string
+	 */
+	private $data;
+
+	/**
+	 * @param null|string $id
+	 */
+	public function setId(?string $id):void
+	{
+		$this->id = $id;
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getId():?string
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @param string $data
+	 */
+	public function setData(string $data):void
+	{
+		$this->data = $data;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getData():string
+	{
+		return $this->data;
+	}
+
+	/**
+	 * @ORM\PrePersist()
+	 * @ORM\PreUpdate()
+	 */
+	public function updateLastHit():void
+	{
+		$this->lastHit = new \DateTime('now');
+	}
+}
---
+++ DoctrineSessionHandler/SessionData/SessionDataEntityInterface.php
@@ -0,0 +1,38 @@
+<?php
+//
+// +---------------------------------------------------------------------+
+// | CODE INC. SOURCE CODE                                               |
+// +---------------------------------------------------------------------+
+// | Copyright (c) 2017 - Code Inc. SAS - All Rights Reserved.           |
+// | Visit https://www.codeinc.fr for more information about licensing.  |
+// +---------------------------------------------------------------------+
+// | NOTICE:  All information contained herein is, and remains the       |
+// | property of Code Inc. SAS. The intellectual and technical concepts  |
+// | contained herein are proprietary to Code Inc. SAS are protected by  |
+// | trade secret or copyright law. Dissemination of this information or |
+// | reproduction of this material  is strictly forbidden unless prior   |
+// | written permission is obtained from Code Inc. SAS.                  |
+// +---------------------------------------------------------------------+
+//
+// Author:   Joan Fabrégat <joan@codeinc.fr>
+// Date:     08/03/2018
+// Time:     17:30
+// Project:  lib-doctrinesessionhandler
+//
+declare(strict_types = 1);
+namespace CodeInc\DoctrineSessionHandler\SessionData;
+
+
+/**
+ * Interface SessionDataEntityInterface
+ *
+ * @see SessionDataEntity
+ * @package CodeInc\Session
+ * @author Joan Fabrégat <joan@codeinc.fr>
+ */
+interface SessionDataEntityInterface {
+	public function setId(string $sessionId):void;
+	public function getId():?string;
+	public function setData(string $data):void;
+	public function getData():?string;
+}
